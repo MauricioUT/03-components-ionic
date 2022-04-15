f93ea5e5468932d6b6f648a0bf0d127ac2172192
Mauricio Utrilla <mutrilla@customsoft.biz>
2022-04-15 14:12:54 -0500

    implementacion de ion-action-sheet

--- src/app/pages/action-sheet/action-sheet.page.ts
+++ src/app/pages/action-sheet/action-sheet.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-action-sheet',
@@ -7,9 +8,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ActionSheetPage implements OnInit {
 
-  constructor() { }
+  constructor(public actionSheetController: ActionSheetController) { }
 
   ngOnInit() {
   }
+  onClick() {
+    this.presentActionSheet();
+  }
 
+  async presentActionSheet() {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Albumes',
+      backdropDismiss: false,// non se podrá cerrar el action-sheet si se da click fuera 
+      buttons: [{
+        text: 'Delete',
+        role: 'destructive',
+        icon: 'trash-outline',
+        cssClass:'rojo', // se implementa una clase en del global.scss
+        id: 'delete-button',
+        data: {
+          type: 'delete'
+        },
+        handler: () => {
+          console.log('Delete clicked');
+        }
+      }, {
+        text: 'Share',
+        icon: 'share-outline',
+        data: 10,
+        handler: () => {
+          console.log('Share clicked');
+        }
+      }, {
+        text: 'Play (open modal)',
+        icon: 'caret-forward-circle-outline',
+        data: 'Data value',
+        handler: () => {
+          console.log('Play clicked');
+        }
+      }, {
+        text: 'Favorite',
+        icon: 'heart-outline',
+        handler: () => {
+          console.log('Favorite clicked');
+        }
+      }, {
+        text: 'Cancel',
+        icon: 'close-outline',
+        role: 'cancel',
+        handler: () => {
+          console.log('Cancel clicked');
+        }
+      }]
+    });
+    await actionSheet.present();
+
+    const { role, data } = await actionSheet.onDidDismiss();
+    console.log('onDidDismiss resolved with role and data', role, data);
+  }
 }
